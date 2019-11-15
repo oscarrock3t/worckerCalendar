f279d713366113e5159ae87850e35fb82be8b271
Kirill <tihankinkirill@gmail.com>
2019-11-15 19:08:19 +0500

    1

--- scripts/main.js
+++ scripts/main.js
@@ -5,58 +5,61 @@ var	nameMonth = ['Январь','Февраль','Март','Апрель','Ма
 
 window.onload = () => {
 	renderMonth();
+	currentTime();
 	currentDate();
 }
+//--------------------------------------------------
 
 setInterval(function() {
-currentDate();
-},1000);
+currentTime();
+},1000); 
 
 function currentDate() {
-		date = new Date(),
-		currentDay = date.getDate(),
-		currentMonth = date.getMonth(),
-		currentYear = parseInt('20'+date.getYear()%100),
-		currentHour = beautifyNumbers(date.getHours()),
-		currentMinute = beautifyNumbers(date.getMinutes()),
-		currentSecond = beautifyNumbers(date.getSeconds()),
-		nameToday = date.getDay();
-
-		document.querySelector('.today-day').innerHTML = `${nameDays[nameToday]}, ${currentDay} ${monthToday()} ${currentYear}`;
-
-		document.querySelector('.today-time').innerHTML = `${currentHour}:${currentMinute}:${currentSecond}`;
+	currentDay = date.getDate(),
+	currentMonth = date.getMonth(),
+	currentYear = date.getYear()%100,
+	nameToday = date.getDay();
+	document.querySelector('.today-day').innerHTML = `${nameDays[nameToday]}, ${currentDay} ${monthToday()} 20${currentYear}`;
+	function monthToday() {
+		return currentMonth == 2 || currentMonth == 7 ?	nameMonth[currentMonth] + 'а' :	nameMonth[currentMonth].slice(0,-1) + 'я';
+	};
 };
-function beautifyNumbers(i) {
-	if (i < 10) {
-		return i = '0' + i;
-	} else {
-		return i;
-	}
+
+function currentTime() {
+	date = new Date(),
+	currentHour = beautifyNumbers(date.getHours()),
+	currentMinute = beautifyNumbers(date.getMinutes()),
+	currentSecond = beautifyNumbers(date.getSeconds());
+	document.querySelector('.today-time').innerHTML = `${currentHour}:${currentMinute}:${currentSecond}`;
+	function beautifyNumbers(i) {
+		return i < 10 ?  '0' + i : i;
+	};	
 };
+//----------------------------------------------
 function renderMonth() {
 	monthes.forEach((e, i) => {
 	e.querySelector('.month-title').append(nameMonth[i]);
 	let monthLength = new Date(parseInt('20'+stateDate.getYear()%100),i,0);
 	for (let j = 1; j < monthLength.getDay() + 1 ;j++) {
 		e.querySelector('.month-days').innerHTML += '<span></span>';
+		console.log();
 	}	
 	monthLength.setMonth(i+1,0);
 	for (let j = 1; j <= monthLength.getDate(); j++) {
-
-		e.querySelector('.month-days').innerHTML += `<p>${j}</p>`;
+		e.querySelector('.month-days').innerHTML += `<p class='day'>${j}</p>`;
 	}	
 	});
 	todayDay();
 };
-function monthToday() {
-		if (currentMonth == 2 || currentMonth == 7) {
-			return nameMonth[currentMonth] + 'а';
-		} else {
-			return nameMonth[currentMonth].slice(0,-1) + 'я';
-		}	
-};
 
 function todayDay() {
 	today = monthes[stateDate.getMonth()].querySelector('.month-days').querySelectorAll('p').item(stateDate.getDate()-1);
-	today.className = 'today';
+	monthes[stateDate.getMonth()].className += ' current-month';
+	today.className += ' today';
+}
+//---------------------------------------------------
+
+
+function test() {
+	console.log('123');
 }
